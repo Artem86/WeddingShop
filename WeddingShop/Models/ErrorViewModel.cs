66bc6b7be1423b4752a7c94d89f1d2a3114fe424
Aleksey Knyazev <joalexey@gmail.com>
2018-02-18 16:00:48 +0300

    Initial commit

---
+++ WeddingShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace WeddingShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
